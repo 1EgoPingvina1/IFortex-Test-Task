b03e2efe37dd92ea0daf15e64933b63b9197d27a
User <User@F4632L2384U2475>
2023-10-13 14:31:23 +0300

    Finish Task

---
+++ TestTask/Data/OrderService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Data
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context) => _context = context;
+
+        public async Task<Order> GetOrder() => await _context.Orders.OrderByDescending(x => x.Price).FirstOrDefaultAsync();
+
+        public async Task<List<Order>> GetOrders() 
+            => await _context.Orders
+                .Where(x => x.Quantity > 10)
+                .ToListAsync();
+    }
+}
---
+++ TestTask/Data/UserService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Data
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context) => _context = context;
+        
+        public async Task<User> GetUser()
+        {
+            var UserWithMostCountOrder = await _context.Users.
+                OrderByDescending(x => x.Orders.Count()).FirstOrDefaultAsync();
+            return UserWithMostCountOrder;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var UserInActive = await _context.Users.Where(x => x.Status == Enums.UserStatus.Inactive).ToListAsync();
+            return UserInActive;
+        }
+    }
+}
